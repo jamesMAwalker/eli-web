bf69eba8ddc582015891fe4a641f100f66fc92f5
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2025-09-21 06:24:57 +0700

    feat(base): :lipstick: Update TW configs and add base stylesheets.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,60 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        xs: '475px',
+        lg: '1025px',
+        xl: '1200px', // navbar collapses on this breakpoint
+        xxxl: '1700px', // navbar collapses on this breakpoint
+        '2k': '2200px'
       },
+      // backgroundImage: {
+      //   'site': "url('/noise-bg.png')",
+      // },
+      colors: {
+        primary: {
+          100: 'rgba(var(--color-primary-100) / <alpha-value>)',
+          150: 'rgba(var(--color-primary-150) / <alpha-value>)',
+          300: 'rgba(var(--color-primary-300) / <alpha-value>)',
+          500: 'rgba(var(--color-primary-500) / <alpha-value>)',
+          600: 'rgba(var(--color-primary-600) / <alpha-value>)',
+          900: 'rgba(var(--color-primary-900) / <alpha-value>)'
+        },
+        accent: {
+          300: 'rgba(var(--color-accent-300) / <alpha-value>)'
+        },
+        neutral: {
+          100: 'rgba(var(--color-neutral-100) / <alpha-value)',
+          200: 'rgba(var(--color-neutral-200) / <alpha-value)',
+          300: 'rgba(var(--color-neutral-300) / <alpha-value)',
+          400: 'rgba(var(--color-neutral-400) / <alpha-value)',
+          500: 'rgba(var(--color-neutral-500) / <alpha-value)',
+          600: 'rgba(var(--color-neutral-600) / <alpha-value)',
+          700: 'rgba(var(--color-neutral-700) / <alpha-value)',
+          800: 'rgba(var(--color-neutral-800) / <alpha-value)',
+          900: 'rgba(var(--color-neutral-900) / <alpha-value)'
+        },
+      },
+      gap: {
+        xs: '5px',
+        sm: '10px',
+        ms: '15px',
+        md: '25px',
+        ml: '40px',
+        lg: '50px',
+        xl: '75px',
+        xxl: '100px'
+      },
+      spacing: {
+        xs: '5px',
+        sm: '10px',
+        ms: '15px',
+        md: '25px',
+        ml: '40px',
+        lg: '50px',
+        xl: '75px',
+        xxl: '100px'
+      }
     },
   },
   plugins: [],
